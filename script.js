a63ab206831df2a692638e327eb35a2d65173206
jaberamin9 <jaberamin9@gmail.com>
2024-04-29 11:17:53 +0600

    v0.08

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 const inputBox = document.getElementById('inputBox')
+const submitBtn = document.getElementById('submit-btn')
+const error = document.getElementById('error')
+const listContainer = document.getElementById('list')
 
 
 const addMoney = () => {
-    const money = parseFloat(parseFloat(inputBox.value).toFixed(2)) || 0
-    console.log(money)
-    if (money != 0) {
-        const moneyList = JSON.parse(localStorage.getItem('money')) || []
-        moneyList.push(money)
-        localStorage.setItem('money', JSON.stringify(moneyList))
+    const money = parseFloat(parseFloat(inputBox.value).toFixed(2))
 
-
-        if (money > 0) {
-            let totalIncome = parseFloat(parseFloat(localStorage.getItem('totalIncome')).toFixed(2)) || 0
-            totalIncome += money
-            localStorage.setItem('totalIncome', parseFloat(totalIncome.toFixed(2)))
-
-            updateValue(document.getElementById('incomeTxt'), parseFloat(totalIncome.toFixed(2)), 'Income: ')
-
-        } else {
-            let totalExpense = parseFloat(localStorage.getItem('totalExpense')) || 0
-            totalExpense += money
-            localStorage.setItem('totalExpense', parseFloat(totalExpense.toFixed(2)))
-
-            updateValue(document.getElementById('expenseTxt'), parseFloat((totalExpense * -1).toFixed(2)), 'Expense: ')
-        }
-
-        updateList(moneyList)
+    if (isNaN(money) || money === 0) {
+        error.style.visibility = 'visible'
+        error.innerText = money === 0 ? 'Sorry, we cannot insert only 0 and 0.00...' : 'Please enter any number'
+        inputBox.value = ''
+        inputBox.focus()
+        return
     }
+
+    error.style.visibility = 'hidden'
+    const moneyList = JSON.parse(localStorage.getItem('money')) || []
+    moneyList.push(money)
+    localStorage.setItem('money', JSON.stringify(moneyList))
+
+
+    if (money > 0) {
+        let totalIncome = parseFloat(parseFloat(localStorage.getItem('totalIncome')).toFixed(2)) || 0
+        totalIncome += money
+        localStorage.setItem('totalIncome', parseFloat(totalIncome.toFixed(2)))
+
+        updateValue(document.getElementById('incomeTxt'), parseFloat(totalIncome.toFixed(2)), 'Income: ')
+
+    } else {
+        let totalExpense = parseFloat(localStorage.getItem('totalExpense')) || 0
+        totalExpense += money
+        localStorage.setItem('totalExpense', parseFloat(totalExpense.toFixed(2)))
+
+        updateValue(document.getElementById('expenseTxt'), parseFloat((totalExpense * -1).toFixed(2)), 'Expense: ')
+    }
+
+    updateList(moneyList)
+
     inputBox.value = ''
+    inputBox.focus()
+
+    listContainer.lastElementChild.scrollIntoView({ behavior: 'smooth', block: 'end' })
 }
+
+submitBtn.addEventListener('click', addMoney)
+inputBox.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+        addMoney()
+    }
+})
 
 
 const updateValue = (element, total, type) => {
@@ -36,155 +57,150 @@ const updateValue = (element, total, type) => {
 }
 
 
-const listElement = (value, idx) => {
-    let node_1 = document.createElement('li');
-    node_1.setAttribute('class', `flex gap-2 items-center p-2 rounded-md ${value > 0 ? 'bg-green-100 hover:bg-green-200' : 'bg-red-100 hover:bg-red-200'}`);
-
-    let node_2 = document.createElement('div');
-    node_2.setAttribute('class', 'flex-1 flex gap-2 items-center');
-    node_1.appendChild(node_2);
-
-    let node_3 = document.createElement('p');
-    node_3.setAttribute('class', 'text-3xl font-normal');
-    node_2.appendChild(node_3);
-
+const deleteItem = (value, idx) => {
+    const moneyList = JSON.parse(localStorage.getItem('money')) || []
 
     if (value > 0) {
-        let node_4 = document.createTextNode(value);
-        node_3.appendChild(node_4);
-        let node_5 = document.createElement('p');
-        node_2.appendChild(node_5);
-
-        let node_6 = document.createTextNode((new String("income")));
-        node_5.appendChild(node_6);
+        let totalIncome = parseFloat(parseFloat(localStorage.getItem('totalIncome')).toFixed(2)) || 0
+        totalIncome -= value
+        localStorage.setItem('totalIncome', parseFloat(totalIncome.toFixed(2)))
+        updateValue(document.getElementById('incomeTxt'), parseFloat(totalIncome.toFixed(2)), 'Income: ')
     } else {
-        let node_4 = document.createTextNode(value * -1);
-        node_3.appendChild(node_4);
-        let node_5 = document.createElement('p');
-        node_2.appendChild(node_5);
-
-        let node_6 = document.createTextNode((new String("expense")));
-        node_5.appendChild(node_6);
+        let totalExpense = parseFloat(parseFloat(localStorage.getItem('totalExpense')).toFixed(2)) || 0
+        totalExpense -= value
+        localStorage.setItem('totalExpense', parseFloat(totalExpense.toFixed(2)))
+        updateValue(document.getElementById('expenseTxt'), parseFloat((totalExpense * -1).toFixed(2)), 'Expense: ')
     }
 
+    moneyList.splice(idx, 1)
+    localStorage.setItem('money', JSON.stringify(moneyList))
+    updateList(moneyList)
+}
 
-    let node_7 = document.createElement('div');
-    node_7.setAttribute('class', 'flex-1 flex gap-3 p- justify-end');
-    node_1.appendChild(node_7);
+const updateItem = (value, idx, editMoneyEtx) => {
+    const updateValue = parseFloat(parseFloat(editMoneyEtx.value).toFixed(2))
+    if (isNaN(updateValue) || updateValue === 0) {
+        error.style.visibility = 'visible'
+        error.innerText = updateValue === 0 ? 'Sorry we can not insert only 0 and 0.00...' : 'Please enter any number'
+        return
+    }
+    error.style.visibility = 'hidden'
 
-    let node_8 = document.createElement('button');
-    node_8.setAttribute('class', 'px-5 py-2 bg-white text-black font-semibold hover:bg-gray-200 rounded-md');
-    node_8.onclick = function () {
-        const moneyList = JSON.parse(localStorage.getItem('money')) || []
+    const moneyList = JSON.parse(localStorage.getItem('money')) || []
+
+    if (updateValue > 0) {
+        let totalIncome = parseFloat(localStorage.getItem('totalIncome')) || 0
+
+        if (value <= 0) {
+            let totalExpense = parseFloat(localStorage.getItem('totalExpense')) || 0
+            totalExpense -= value
+            totalExpense = parseFloat(totalExpense.toFixed(2))
+            localStorage.setItem('totalExpense', totalExpense)
+            document.getElementById('expenseTxt').innerText = 'Expense: ' + (totalExpense * -1)
+        } else {
+            totalIncome -= value
+        }
+        totalIncome += updateValue
+        totalIncome = parseFloat(totalIncome.toFixed(2))
+        localStorage.setItem('totalIncome', totalIncome)
+        document.getElementById('incomeTxt').innerText = 'Income: ' + totalIncome
+    } else {
+        let totalExpense = parseFloat(localStorage.getItem('totalExpense')) || 0
 
         if (value > 0) {
-            let totalIncome = parseFloat(parseFloat(localStorage.getItem('totalIncome')).toFixed(2)) || 0
-            console.log("toatal ", totalIncome)
-
+            let totalIncome = parseFloat(localStorage.getItem('totalIncome')) || 0
             totalIncome -= value
-            localStorage.setItem('totalIncome', parseFloat(totalIncome.toFixed(2)))
-            updateValue(document.getElementById('incomeTxt'), parseFloat(totalIncome.toFixed(2)), 'Income: ')
+            totalIncome = parseFloat(totalIncome.toFixed(2))
+            localStorage.setItem('totalIncome', totalIncome)
+            document.getElementById('incomeTxt').innerText = 'Income: ' + totalIncome
         } else {
-            let totalExpense = parseFloat(parseFloat(localStorage.getItem('totalExpense')).toFixed(2)) || 0
             totalExpense -= value
-            localStorage.setItem('totalExpense', parseFloat(totalExpense.toFixed(2)))
-            updateValue(document.getElementById('expenseTxt'), parseFloat((totalExpense * -1).toFixed(2)), 'Expense: ')
         }
-
-        moneyList.splice(idx, 1)
-        localStorage.setItem('money', JSON.stringify(moneyList))
-        updateList(moneyList)
+        totalExpense += updateValue
+        totalExpense = parseFloat(totalExpense.toFixed(2))
+        localStorage.setItem('totalExpense', totalExpense)
+        document.getElementById('expenseTxt').innerText = 'Expense: ' + (totalExpense * -1)
     }
-    node_7.appendChild(node_8);
 
-    let node_9 = document.createTextNode((new String("Delete")));
-    node_8.appendChild(node_9);
+    moneyList.splice(idx, 1, updateValue)
 
-    let node_10 = document.createElement('button');
-    node_10.setAttribute('class', 'px-5 py-2 bg-white text-black font-semibold hover:bg-gray-200 rounded-md');
-    node_10.onclick = function () {
-        node_2.innerHTML = ''
-        let node_3 = document.createElement('input');
-        node_3.setAttribute('class', 'p-2 rounded-md');
-        node_3.setAttribute('type', 'number')
-        node_3.setAttribute('value', value)
-        node_2.appendChild(node_3);
+    localStorage.setItem('money', JSON.stringify(moneyList))
+    updateList(moneyList)
+}
 
-        node_7.innerHTML = ''
-        let node_8 = document.createElement('button');
-        node_8.setAttribute('class', 'px-5 py-2 bg-white text-black font-semibold hover:bg-gray-200 rounded-md');
-        node_8.onclick = function () {
-            const updateValue = parseFloat(parseFloat(node_3.value).toFixed(2))
-            if (updateValue != 0) {
-                const moneyList = JSON.parse(localStorage.getItem('money')) || []
+const listElement = (value, idx) => {
+    let list = document.createElement('li');
+    list.setAttribute('class', `flex gap-2 items-center p-2 rounded-md ${value > 0 ? 'bg-green-100 hover:bg-green-200' : 'bg-red-100 hover:bg-red-200'}`);
+    //list.classList.add('flex', 'gap-2', 'items-center', 'p-2', 'rounded-md', value > 0 ? 'bg-green-100' : 'bg-red-100', value > 0 ? 'hover:bg-green-200' : 'hover:bg-red-200')
 
-                if (updateValue > 0) {
-                    let totalIncome = parseFloat(localStorage.getItem('totalIncome')) || 0
+    let leftContainer = document.createElement('div');
+    leftContainer.setAttribute('class', 'flex-1 flex gap-2 items-center');
+    list.appendChild(leftContainer);
 
-                    if (value <= 0) {
-                        let totalExpense = parseFloat(localStorage.getItem('totalExpense')) || 0
-                        totalExpense -= value
-                        totalExpense = parseFloat(totalExpense.toFixed(2))
-                        localStorage.setItem('totalExpense', totalExpense)
-                        document.getElementById('expenseTxt').innerText = 'Expense: ' + (totalExpense * -1)
-                    } else {
-                        totalIncome -= value
-                    }
-                    totalIncome += updateValue
-                    totalIncome = parseFloat(totalIncome.toFixed(2))
-                    localStorage.setItem('totalIncome', totalIncome)
-                    document.getElementById('incomeTxt').innerText = 'Income: ' + totalIncome
-                } else {
-                    let totalExpense = parseFloat(localStorage.getItem('totalExpense')) || 0
+    let moneyTxt = document.createElement('p');
+    moneyTxt.setAttribute('class', 'text-3xl font-normal');
+    leftContainer.appendChild(moneyTxt);
 
-                    if (value > 0) {
-                        let totalIncome = parseFloat(localStorage.getItem('totalIncome')) || 0
-                        totalIncome -= value
-                        totalIncome = parseFloat(totalIncome.toFixed(2))
-                        localStorage.setItem('totalIncome', totalIncome)
-                        document.getElementById('incomeTxt').innerText = 'Income: ' + totalIncome
-                    } else {
-                        totalExpense -= value
-                    }
-                    totalExpense += updateValue
-                    totalExpense = parseFloat(totalExpense.toFixed(2))
-                    localStorage.setItem('totalExpense', totalExpense)
-                    document.getElementById('expenseTxt').innerText = 'Expense: ' + (totalExpense * -1)
-                }
+    if (value > 0) {
+        moneyTxt.innerHTML = value
+        let type = document.createElement('p');
+        leftContainer.appendChild(type);
+        type.innerText = 'income'
+    } else {
+        moneyTxt.innerHTML = value
+        let type = document.createElement('p');
+        leftContainer.appendChild(type);
+        type.innerText = 'expense'
+    }
 
-                moneyList.splice(idx, 1, updateValue)
+    let rightContainer = document.createElement('div');
+    rightContainer.setAttribute('class', 'flex-1 flex gap-3 p- justify-end');
+    list.appendChild(rightContainer);
 
-                localStorage.setItem('money', JSON.stringify(moneyList))
-                updateList(moneyList)
+    let deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'px-5 py-2 bg-white text-black font-semibold hover:bg-gray-200 rounded-md');
+    deleteBtn.onclick = () => deleteItem(value, idx)
+    rightContainer.appendChild(deleteBtn);
+    deleteBtn.innerText = 'Delete'
+
+    let editBtn = document.createElement('button');
+    editBtn.setAttribute('class', 'px-5 py-2 bg-white text-black font-semibold hover:bg-gray-200 rounded-md');
+    editBtn.onclick = function () {
+        leftContainer.innerHTML = ''
+        let editMoneyEtx = document.createElement('input')
+        editMoneyEtx.setAttribute('class', 'p-2 rounded-md')
+        editMoneyEtx.setAttribute('type', 'number')
+        editMoneyEtx.setAttribute('value', value)
+        leftContainer.appendChild(editMoneyEtx)
+        editMoneyEtx.focus()
+
+        rightContainer.innerHTML = ''
+        let updateBtn = document.createElement('button');
+        updateBtn.setAttribute('class', 'px-5 py-2 bg-white text-black font-semibold hover:bg-gray-200 rounded-md');
+        updateBtn.onclick = () => updateItem(value, idx, editMoneyEtx)
+        editMoneyEtx.addEventListener('keypress', event => {
+            if (event.key === 'Enter') {
+                updateItem(value, idx, editMoneyEtx)
             }
-        }
-        node_7.appendChild(node_8);
-        let node_9 = document.createTextNode((new String("Update")));
-        node_8.appendChild(node_9);
-
-
+        })
+        rightContainer.appendChild(updateBtn)
+        updateBtn.innerText = 'Update'
     }
-    node_7.appendChild(node_10);
-
-    let node_11 = document.createTextNode((new String("Edit")));
-    node_10.appendChild(node_11);
-
-
-    const element = document.getElementById('list')
-    element.appendChild(node_1)
+    rightContainer.appendChild(editBtn)
+    editBtn.innerText = 'Edit'
+    listContainer.appendChild(list)
 }
 
 
 const updateList = (moneyList) => {
-    const element = document.getElementById('list')
-    element.innerHTML = ''
-    if (moneyList.length === 0) element.innerHTML = `
-    <div class="w-full h-full flex justify-center items-center">
-    <P class="text-black font-medium text-md">Not yet inserted any balance.</P>
-    </div>`
-    moneyList.map((item, idx) => {
-        listElement(item, idx)
-    })
+    listContainer.innerHTML = ''
+    if (moneyList.length === 0) {
+        listContainer.innerHTML = `
+        <div class="w-full h-full flex justify-center items-center" >
+            <P class="text-black font-medium text-md">Not yet inserted any balance.</P>
+        </div > `
+    }
+    moneyList.map(listElement)
 }
 
 
